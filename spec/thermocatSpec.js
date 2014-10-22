9e8bd68f474af34edc0c8abea443e56bc2ec9c85
NicolePell <pellicena@gmail.com>
2014-10-22 10:57:02 +0100

    added power save mode test

--- spec/thermocatSpec.js
+++ spec/thermocatSpec.js
@@ -2,11 +2,18 @@
 
 describe('Thermocat', function(){
 
+	var thermocat;
+
   describe('Default settings', function(){
 
     it('start at 20 degrees', function(){
       thermocat = new Thermocat;
       expect(thermocat.temperature).toEqual(20);
+    });
+
+    it('power save mode should be on', function() {
+    	thermocat = new Thermocat;
+    	expect(thermocat.isPowerSaveMode).toBe(true);
     });
 
 
---
+++ src/thermocat.js
@@ -0,0 +1,5 @@
+function Thermocat() {
+	this.temperature = 20;
+
+
+};
